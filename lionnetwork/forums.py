82c6fd9454234d267a1423b070d2bd698bfcc933
Harshini,R <harshiniramanujam@gmail.com>
2022-11-12 20:31:26 -0500

    fix all forums related queries

--- lionnetwork/forums.py
+++ lionnetwork/forums.py
@@ -7,11 +7,19 @@ from lionnetwork.auth import login_required
 
 forums = Blueprint('forums', __name__, url_prefix='/forums')
 
+
+def fetch_industries():
+    industries = g.conn.execute(
+        'select * from industries'
+    ).fetchall()
+    return industries
+
+
 @forums.route('/')
 def index():
     forums = g.conn.execute(
-        'SELECT *'
-        ' FROM forums'
+        'SELECT f.forum_id, f.forum_name, f.forum_description, f.columbia_uni, i.industry_name'
+        ' FROM forums f join industries i on f.industry_id = i.industry_id'
         ' ORDER BY forum_name'
     ).fetchall()
     session['forum_id'] = ""
@@ -22,40 +30,43 @@ def index():
 @login_required
 def create():
     if request.method == 'POST':
-        name = request.form['name']
-        description = request.form['description']
+        forum_name = request.form['forum_name']
+        forum_description = request.form['forum_description']
+        
         error = None
 
-        if not name:
+        print(forum_name)
+        industry_id = request.form['industry_id']
+
+        if not forum_name:
             error = 'Forum name is required.'
+        if not industry_id:
+            error = 'Industry name is required.'
 
         if error is not None:
             flash(error)
         else:
             g.conn.execute(
-                'INSERT INTO forums (forum_name, forum_description, columbia_uni)'
-                ' VALUES (%s, %s, %s)',
-                [name, description, g.user['columbia_uni']]
+                'INSERT INTO forums (forum_name, forum_description, columbia_uni, industry_id)'
+                ' VALUES (%s, %s, %s, %s)',
+                [forum_name, forum_description, g.user['columbia_uni'], industry_id]
             )
             return redirect(url_for('forums.index'))
-
-    return render_template('forums/create.html')
+    
+    
+    return render_template('forums/create.html', industries=fetch_industries())
 
 
 def get_forum(id, check_admin=True):
     forum = g.conn.execute(
-        'SELECT *'
-        ' FROM forums'
-        ' WHERE forum_id = %s',
+        'SELECT f.forum_id, f.forum_name, f.forum_description, f.columbia_uni, i.industry_name'
+        ' FROM forums f join industries i on f.industry_id = i.industry_id'
+        ' WHERE f.forum_id = %s',
         [id,]
     ).fetchone()
 
     if forum is None:
         abort(404, f"Forum id {id} doesn't exist.")
-
-    #TODO modify below check condition for checking if user is admin
-    # if !check_admin:
-    #     abort(403)
 
     return forum
 
@@ -68,6 +79,7 @@ def update(id):
     if request.method == 'POST':
         name = request.form['forum_name']
         description = request.form['forum_description']
+        industry_id = request.form['industry_id']
         error = None
 
         if not name:
@@ -77,13 +89,13 @@ def update(id):
             flash(error)
         else:
             g.conn.execute(
-                'UPDATE forums SET forum_name = %s, forum_description = %s'
+                'UPDATE forums SET forum_name = %s, forum_description = %s, industry_id = %s'
                 ' WHERE forum_id = %s',
-                (name, description, id)
+                (name, description, industry_id, id)
             )
             return redirect(url_for('forums.index'))
 
-    return render_template('forums/update.html', forum=forum)
+    return render_template('forums/update.html', forum=forum, industries=fetch_industries())
 
 
 @forums.route('/<int:id>/delete', methods=('POST',))
--- lionnetwork/posts.py
+++ lionnetwork/posts.py
@@ -7,6 +7,7 @@ from lionnetwork.auth import login_required
 
 posts = Blueprint('posts', __name__, url_prefix='/posts')
 
+
 def get_forum(id):
     forum = g.conn.execute(
         'SELECT *'
@@ -19,6 +20,7 @@ def get_forum(id):
         abort(404, f"Forum id {id} doesn't exist.")
 
     return forum['forum_name']
+
 
 @posts.route('/<int:id>', methods=('GET',))
 @login_required
@@ -35,7 +37,6 @@ def index(id):
 def create():
     if request.method == 'POST':
         post_text = request.form['post_text']
-        post_id = request.form['post_id']
         forum_id = session['forum_id']
         if forum_id == None:
             flash("Invalid state!")
@@ -49,13 +50,14 @@ def create():
             flash(error)
         else:
             g.conn.execute(
-                'INSERT INTO posts (post_id, forum_id, post_text, columbia_uni, timestamp_posted)'
-                ' VALUES (%s, %s, %s, %s)',
-                [post_id, forum_id, post_text, g.user['columbia_uni'], ]
+                'INSERT INTO posts (forum_id, post_text, columbia_uni)'
+                ' VALUES (%s, %s, %s)',
+                [forum_id, post_text, g.user['columbia_uni'], ]
             )
             return redirect(url_for('posts.index', id=session['forum_id']))
 
     return render_template('posts/create.html')
+
 
 def get_post(id):
     post = g.conn.execute(
@@ -71,14 +73,48 @@ def get_post(id):
     return post
 
 
+def get_comment(id):
+    comment = g.conn.execute(
+        'SELECT *'
+        ' FROM comments'
+        ' WHERE comment_id = %s',
+        [id,]
+    ).fetchone()
+
+    if comment is None:
+        abort(404, f"Comment id {id} doesn't exist.")
+
+    return comment
+
+
 @posts.route('/view/<int:id>', methods=('GET',))
 def show(id):
-    print("in here?")
     post = get_post(id)
     comments = g.conn.execute(
-        'SELECT c.comment_text, c.timestamp_posted, c.columbia_uni as poster FROM comments c JOIN posts p on c.post_id = p.post_id and c.forum_id = p.forum_id WHERE p.post_id = %s and p.forum_id = %s', (id, session['forum_id'])
+        'SELECT c.comment_id, c.comment_text, c.timestamp_posted, c.columbia_uni as poster FROM comments c JOIN posts p on c.post_id = p.post_id and c.forum_id = p.forum_id WHERE p.post_id = %s and p.forum_id = %s', (id, session['forum_id'])
     ).fetchall()
     return render_template('posts/show.html', post=post, comments=comments)
+
+
+@posts.route('/<int:id>/comment', methods=('POST',))
+@login_required
+def comment(id):
+    comment_text = request.form['comment_text']
+    g.conn.execute(
+        'INSERT INTO comments (forum_id, post_id, comment_text, columbia_uni)'
+        ' VALUES (%s, %s, %s, %s)',
+        [session["forum_id"], id, comment_text, g.user['columbia_uni'], ]
+    )
+    return redirect(url_for('posts.show', id=id))
+
+
+@posts.route('/<int:id>/comment/delete', methods=('POST',))
+@login_required
+def comment_delete(id):
+    print("in here?")
+    comment = get_comment(id)
+    g.conn.execute('DELETE FROM comments WHERE comment_id = %s', (id,))
+    return redirect(url_for('posts.show', id=comment['post_id']))
 
 
 @posts.route('/<int:id>/update', methods=('GET', 'POST'))
@@ -86,20 +122,19 @@ def show(id):
 def update(id):
     post = get_post(id)
     if request.method == 'POST':
-        name = request.form['forum_name']
-        description = request.form['forum_description']
+        post_text = request.form['post_text']
         error = None
 
-        if not name:
-            error = 'Forum name is required.'
+        if not post_text:
+            error = 'Post text is required.'
 
         if error is not None:
             flash(error)
         else:
             g.conn.execute(
-                'UPDATE forums SET forum_name = %s, forum_description = %s'
-                ' WHERE forum_id = %s',
-                (name, description, id)
+                'UPDATE posts SET post_text = %s'
+                ' WHERE post_id = %s AND forum_id = %s',
+                (post_text, id, session['forum_id'])
             )
             return redirect(url_for('posts.index', id=session['forum_id']))
     return render_template('posts/update.html', post=post)
